ab738e709ad9aed9a9c39fc9736daf1159278738
Javier O. Cordero Pérez <javier.cordero@kdab.com>
2024-01-09 15:11:08 -0400

    Add KDE Frameworks 5 Kirigami package

---
+++ ports/kf5kirigami/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kirigami
+    REF v5.98.0
+    SHA512 2d0c7c332fd01850eefd5b68ce61d69bd41f3e856ac82a63407c272a0d4b315ded12c01583b7d73b46e05a596385090fb9bf5476483f9d5aa26cb9a77a8cb72d
+)
+
+# Prevent KDEClangFormat from writing to source effectively blocking parallel configure
+file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DKDE_INSTALL_QMLDIR=qml
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Kirigami CONFIG_PATH lib/cmake/KF5Kirigami)
+vcpkg_copy_pdbs()
+
+file(APPEND "${CURRENT_PACKAGES_DIR}/tools/${PORT}/qt.conf" "Data = ../../share")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
+vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
+
